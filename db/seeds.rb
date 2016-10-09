c60236df40fad4aa83ccb4461ce93e03fa45f2f0
Colin Kingen <colinwkingen@gmail.com>
2016-10-09 16:26:17 -0700

    create basic react on rails project with JSON and most crud functions

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Item.create({name: 'Chowder', description: 'Creamy Soup'})
+Item.create({name: 'Curry', description: 'Curried Soup'})
+Item.create({name: 'Tom Kha', description: 'Coconut Soup'})
+Item.create({name: 'Noodle', description: 'Classic Soup'})
+Item.create({name: 'Split Pea', description: 'Pea Soup'})
+Item.create({name: 'Lobster Bisque', description: 'Seafood Soup'})
+Item.create({name: 'Minestrone', description: 'Tomato based Soup'})
+Item.create({name: 'Tomato Basil', description: 'Good with Sandwich'})
