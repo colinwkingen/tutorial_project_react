0c4d12574d6c00772bbbed570c8cddcaad3303b2
Colin Kingen <colinwkingen@gmail.com>
2016-10-09 21:22:19 -0700

    finish all crud functionality with ajax

--- app/assets/javascripts/components/_item.js.jsx
+++ app/assets/javascripts/components/_item.js.jsx
@@ -1,15 +1,29 @@
 var Item = React.createClass({
+  getInitialState() {
+    return {editable: false}
+  },
+
   handleEdit() {
-    console.log('edit button clicked')
+    if(this.state.editable) {
+      var name = this.refs.name.value;
+      var id = this.props.item.id;
+      var description = this.refs.description.value;
+      var item = {id: id, name: name, description: description};
+      this.props.handleUpdate(item);
+      console.log('in handleEdit', this.state.editable, name, description, id);
+    }
+    this.setState({ editable: !this.state.editable })
   },
 
   render() {
-    render (
+    var name = this.state.editable ? <input type='text' ref='name' defaultValue={this.props.item.name} /> : <h3>{this.props.item.name}</h3>;
+    var description = this.state.editable ? <input type='text' ref='description' defaultValue={this.props.item.description} /> : <p>{this.props.item.description}</p>;
+    return (
       <div>
-        <h3>{this.props.item.name}</h3>
-        <p>{this.props.item.description}</p>
+        {name}
+        {description}
         <button onClick={this.props.handleDelete}> Delete </button>
-        <button onClick={this.handleEdit}> Edit </button>
+        <button onClick={this.handleEdit}> {this.state.editable ? 'Submit' : 'Edit' }</button>
       </div>
     )
   }
